51135e62fc686772119a6f5952ab4e928d3560ef
PedroHumberto <78572029+PedroHumberto@users.noreply.github.com>
2021-02-25 14:39:33 -0300

    Add files via upload

---
+++ URI1010 VETOR/URI1010 VETOR/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI1010_VETOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] peca1 = Console.ReadLine().Split(' ');
+            int codigo = int.Parse(peca1[0]);
+            int qt = int.Parse(peca1[1]);
+            double valor = double.Parse(peca1[2], CultureInfo.InvariantCulture);
+
+            string[] peca2 = Console.ReadLine().Split(' ');
+            int codigo2 = int.Parse(peca2[0]);
+            int qt2 = int.Parse(peca2[1]);
+            double valor2 = double.Parse(peca2[2], CultureInfo.InvariantCulture);
+
+            double soma = valor * qt + valor2 * qt2;
+
+            Console.WriteLine("VALOR A PAGAR: R$ " + soma.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+        }
+    }
+}
